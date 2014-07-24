846b8dbbabdc39762f117bdde63d8d5a526d8ca9
Tommy Ryan <tommy@thetallboy.com>
2014-07-24 11:03:53 -0700

    URL fixes

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ javascript:(function(document){
 		var jQuery = document.createElement('script');
 		jQuery.setAttribute('type', 'text/javascript');
 		jQuery.setAttribute('charset', 'UTF-8');
-		jQuery.setAttribute('src', 'http://http://code.jquery.com/jquery-1.11.1.min.js');
+		jQuery.setAttribute('src', 'http://code.jquery.com/jquery-1.11.1.min.js');
 		document.body.appendChild(jQuery);
 	}
 
 	var script = document.createElement("script");
-	script.src = "https://raw.github.com/tallboy/faithful/master/faithful.js";
+	script.src = "https://rawgit.github.com/tallboy/faithful/master/faithful.js";
 	document.getElementsByTagName("head")[0].appendChild(script);
 
 }(document));
